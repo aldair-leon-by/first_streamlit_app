ea38cbf95852cb212dc0b850e3c00310da94f5ed
Aldair Leon <65984000+aldair-leon-by@users.noreply.github.com>
2022-05-12 13:45:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_responce.json())
 streamlit.dataframe(fruityvice_normalized)
 
 streamlit.text("Hello from Snowflake:")
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
